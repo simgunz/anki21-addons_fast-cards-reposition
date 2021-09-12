793151bd85fc93e7d0beb5490d607351442fba16
Simone Gaiarin <simgunz@gmail.com>
2021-09-12 13:44:18 +0200

    Refactor extension into a class, add setActionsEnabled

--- __init__.py
+++ __init__.py
@@ -38,144 +38,143 @@ def gc(arg, fail=False):
     else:
         return fail
 
+class FastCardReposition:
+    def __init__(self, browser):
+        self.browser = browser
+        self.actions = []
+        self._setupFastRepositionActions()
+
+    def setActionsEnabled(self, enabled):
+        for action in self.actions:
+           action.setEnabled(enabled)
+
+    def moveCardUp(self):
+        self._moveCard(-1)
+
+    def moveCardDown(self):
+        self._moveCard(1)
+
+    def moveCardToTop(self):
+        #Get only new cards and exit if none are selected
+        cids = self.browser.selectedCards()
+        cids2 = self.browser.col.db.list(
+                "select id from cards where type = 0 and id in " + ids2str(cids))
+        if not cids2:
+            return showInfo("Only new cards can be repositioned.")
+
+        verticalScrollBar = self.browser.form.tableView.verticalScrollBar()
+        scrollBarPosition = verticalScrollBar.value()
+
+        # old repositioning code was removed with https://github.com/ankitects/anki/commit/0331d8b588e2173af33aea3807538f17daf042bb
+        op = reposition_new_cards(parent=self.browser, card_ids=cids, starting_from=0, step_size=1, randomize=0, shift_existing=1)
+        op.run_in_background()
+        self.browser.onSearchActivated()
+        #Update the due position of the next card added.
+        #This guarantees that the new cards are added a the end.
+        self.browser.col.conf['nextPos'] = self.browser.col.db.scalar(
+                "select max(due)+1 from cards where type = 0") or 0
+
+        verticalScrollBar.setValue(scrollBarPosition)
+
+    def _moveCard(self, pos):  # self is browser
+        revs = self.browser.col.conf['sortBackwards']
+        srows = self.browser.table._selected()
+
+        # sanity check
+        if self.browser.table.is_notes_mode():
+            return showInfo("Only works in cards mode.")
+
+        #Get only new cards and exit if none are selected
+        cids = self.browser.selectedCards()
+        cids2 = self.browser.col.db.list(
+                "select id from cards where type = 0 and id in " + ids2str(cids))
+        if not cids2:
+            return showInfo("Only new cards can be repositioned.")
+
+        #Get the list of indexes of the selcted rows
+        srowsidxes = []
+        for crow in srows:
+            srowsidxes.append(crow.row())
+
+        model = self.browser.table._model
+
+        #Check if the first (last) selected row is the first (last) on the table
+        #and return in that case because it cannot moved up (down)
+        if pos == -1:
+            if not self.browser.table.has_previous():
+                return
+            srowidx = min(srowsidxes)
+        elif pos == 1:
+            if not self.browser.table.has_next():
+                return
+            srowidx = max(srowsidxes)
+
+        #Get the index of the card on which the new due is calculated
+        startidx = srowidx+pos
+        #Check that the card on which the new due is calculated is a new card, otherwise the selected
+        #card is at the boundary with the review cards and should not be moved
+        cf = model._items[startidx]
+        cf2 = self.browser.col.db.list(
+                "select id from cards where type = 0 and id in " + ids2str([cf]))
+        if not cf2:
+            return
+
+        #When we move down (up) and the cards are in ascending (descending) order, the new due date must be greater by one
+        #respect the due date of the next (previous) card, otherwise the due date of the selected card will be equal of that of
+        #the next (previous) card but its position will be still before the next (previous) card
+        inc = (revs==0 and pos>0) or (revs==1 and pos<0)
+
+        start=self.browser.col.getCard(cf).due+inc
+
+        # old repositioning code was removed with https://github.com/ankitects/anki/commit/0331d8b588e2173af33aea3807538f17daf042bb
+        op = reposition_new_cards(parent=self.browser, card_ids=cids, starting_from=start, step_size=1, randomize=0, shift_existing=1)
+        op.run_in_background()
+        self.browser.onSearchActivated()
+        #Update the due position of the next card added.
+        #This guarantees that the new cards are added a the end.
+        self.browser.col.conf['nextPos'] = self.browser.col.db.scalar(
+                "select max(due)+1 from cards where type = 0") or 0
+
+    def _setupFastRepositionActions(self):
+        """Add actions to the browser menu to move the cards up and down
+        """
+        # Set the actions active only if the cards are sorted by due date. This is necessary because the reposition
+        # is done considering the current ordering in the browser
+        mvtotopAction = QAction("Move to top", self.browser)
+        mvtotopAction.setShortcut(shortcut(gc("shortcut: Move to top", "Alt+0")))
+        mvtotopAction.triggered.connect(self.moveCardToTop)
+        self.actions.append(mvtotopAction)
+
+        mvuponeAction = QAction("Move one up", self.browser)
+        mvuponeAction.setShortcut(shortcut(gc("shortcut: Move one up", "Alt+Up")))
+        mvuponeAction.triggered.connect(self.moveCardUp)
+        self.actions.append(mvuponeAction)
+
+        mvdownoneAction = QAction("Move one down", self.browser)
+        mvdownoneAction.setShortcut(shortcut(gc("shortcut: Move one down", "Alt+Down")))
+        mvdownoneAction.triggered.connect(self.moveCardDown)
+        self.actions.append(mvdownoneAction)
+
+        self.browser.form.menu_Cards.addSeparator()
+        self.browser.form.menu_Cards.addAction(mvtotopAction)
+        self.browser.form.menu_Cards.addAction(mvuponeAction)
+        self.browser.form.menu_Cards.addAction(mvdownoneAction)
+
+        isDueSort = self.browser.col.conf['sortType'] == 'cardDue'
+        self.setActionsEnabled(isDueSort)
+
 
 def fastRepositionOnSortChanged(self, section, order):
     column = self._model.column_at_section(section)
     isDueSort = column.cards_mode_label == 'Due'
-    self.browser.form.mvtotopAction.setEnabled(isDueSort)
-    self.browser.form.mvuponeAction.setEnabled(isDueSort)
-    self.browser.form.mvdownoneAction.setEnabled(isDueSort)
+    self.browser.fastCardReposition.setActionsEnabled(isDueSort)
 
 
-def setupFastRepositionActions(browser):
-    """Add actions to the browser menu to move the cards up and down
-    """
-    # Set the actions active only if the cards are sorted by due date. This is necessary because the reposition
-    # is done considering the current ordering in the browser
-    mvtotopAction = QAction("Move to top", browser)
-    mvtotopAction.setShortcut(shortcut(gc("shortcut: Move to top", "Alt+0")))
-    mvtotopAction.triggered.connect(browser.moveCardToTop)
+def initFastCardReposition(browser):
+    browser.fastCardReposition = FastCardReposition(browser)
 
-    mvuponeAction = QAction("Move one up", browser)
-    mvuponeAction.setShortcut(shortcut(gc("shortcut: Move one up", "Alt+Up")))
-    mvuponeAction.triggered.connect(browser.moveCardUp)
-
-    mvdownoneAction = QAction("Move one down", browser)
-    mvdownoneAction.setShortcut(shortcut(gc("shortcut: Move one down", "Alt+Down")))
-    mvdownoneAction.triggered.connect(browser.moveCardDown)
-
-    browser.form.mvtotopAction = mvtotopAction
-    browser.form.mvuponeAction = mvuponeAction
-    browser.form.mvdownoneAction = mvdownoneAction
-
-    browser.form.menu_Cards.addSeparator()
-    browser.form.menu_Cards.addAction(mvtotopAction)
-    browser.form.menu_Cards.addAction(mvuponeAction)
-    browser.form.menu_Cards.addAction(mvdownoneAction)
-
-    isDueSort = browser.col.conf['sortType'] == 'cardDue'
-    browser.form.mvtotopAction.setEnabled(isDueSort)
-    browser.form.mvuponeAction.setEnabled(isDueSort)
-    browser.form.mvdownoneAction.setEnabled(isDueSort)
-
-
-def moveCard(self, pos):  # self is browser
-    revs = self.col.conf['sortBackwards']
-    srows = self.table._selected()
-
-    # sanity check
-    if self.table.is_notes_mode():
-        return showInfo("Only works in cards mode.")
-
-    #Get only new cards and exit if none are selected
-    cids = self.selectedCards()
-    cids2 = self.col.db.list(
-            "select id from cards where type = 0 and id in " + ids2str(cids))
-    if not cids2:
-        return showInfo("Only new cards can be repositioned.")
-
-    #Get the list of indexes of the selcted rows
-    srowsidxes = []
-    for crow in srows:
-        srowsidxes.append(crow.row())
-
-    model = self.table._model
-
-    #Check if the first (last) selected row is the first (last) on the table
-    #and return in that case because it cannot moved up (down)
-    if pos == -1:
-        if not self.table.has_previous():
-            return
-        srowidx = min(srowsidxes)
-    elif pos == 1:
-        if not self.table.has_next():
-            return
-        srowidx = max(srowsidxes)
-
-    #Get the index of the card on which the new due is calculated
-    startidx = srowidx+pos
-    #Check that the card on which the new due is calculated is a new card, otherwise the selected
-    #card is at the boundary with the review cards and should not be moved
-    cf = model._items[startidx]
-    cf2 = self.col.db.list(
-            "select id from cards where type = 0 and id in " + ids2str([cf]))
-    if not cf2:
-        return
-
-    #When we move down (up) and the cards are in ascending (descending) order, the new due date must be greater by one
-    #respect the due date of the next (previous) card, otherwise the due date of the selected card will be equal of that of
-    #the next (previous) card but its position will be still before the next (previous) card
-    inc = (revs==0 and pos>0) or (revs==1 and pos<0)
-
-    start=self.col.getCard(cf).due+inc
-
-    # old repositioning code was removed with https://github.com/ankitects/anki/commit/0331d8b588e2173af33aea3807538f17daf042bb
-    op = reposition_new_cards(parent=self, card_ids=cids, starting_from=start, step_size=1, randomize=0, shift_existing=1)
-    op.run_in_background()
-    self.onSearchActivated()
-    #Update the due position of the next card added.
-    #This guarantees that the new cards are added a the end.
-    self.col.conf['nextPos'] = self.col.db.scalar(
-            "select max(due)+1 from cards where type = 0") or 0
-
-
-def moveCardUp(self):
-    self.moveCard(-1)
-
-
-def moveCardDown(self):
-    self.moveCard(1)
-
-
-def moveCardToTop(self):
-    #Get only new cards and exit if none are selected
-    cids = self.selectedCards()
-    cids2 = self.col.db.list(
-            "select id from cards where type = 0 and id in " + ids2str(cids))
-    if not cids2:
-        return showInfo("Only new cards can be repositioned.")
-
-    verticalScrollBar = self.form.tableView.verticalScrollBar()
-    scrollBarPosition = verticalScrollBar.value()
-
-    # old repositioning code was removed with https://github.com/ankitects/anki/commit/0331d8b588e2173af33aea3807538f17daf042bb
-    op = reposition_new_cards(parent=self, card_ids=cids, starting_from=0, step_size=1, randomize=0, shift_existing=1)
-    op.run_in_background()
-    self.onSearchActivated()
-    #Update the due position of the next card added.
-    #This guarantees that the new cards are added a the end.
-    self.col.conf['nextPos'] = self.col.db.scalar(
-            "select max(due)+1 from cards where type = 0") or 0
-
-    verticalScrollBar.setValue(scrollBarPosition)
-
-
-browser.Browser.moveCard = moveCard
-browser.Browser.moveCardUp = moveCardUp
-browser.Browser.moveCardDown = moveCardDown
-browser.Browser.moveCardToTop = moveCardToTop
 
 browser.table.Table._on_sort_column_changed = hooks.wrap(
     browser.table.Table._on_sort_column_changed, fastRepositionOnSortChanged)
 
-hooks.addHook("browser.setupMenus", setupFastRepositionActions)
+hooks.addHook("browser.setupMenus", initFastCardReposition)
