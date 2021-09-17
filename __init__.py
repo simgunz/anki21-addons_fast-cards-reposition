c287026803535471231286757d983b9769510c3c
Simone Gaiarin <simgunz@gmail.com>
2021-09-17 07:57:10 +0200

    Update copyright year

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #########################################################################
-# Copyright (C) 2014 by Simone Gaiarin <simgunz@gmail.com>              #
+# Copyright (C) 2014-2021 by Simone Gaiarin <simgunz@gmail.com>         #
 #                                                                       #
 # This program is free software; you can redistribute it and/or modify  #
 # it under the terms of the GNU General Public License as published by  #
